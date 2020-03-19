6077ecadc330ebdd61b179d53a3f08048bfc05e8
JI-YOUNG <kjy6269@oniontech.com>
2020-03-19 16:21:12 +0900

    Create Customs.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Customer from './components/Customer';
 import './App.css';
 
-function App() {
-  return (
-    <div className="gray-background">
-      <img src={logo} lat="log" />
-      <h2>Let's develop management system !</h2>
-    </div>
-  );
+const customers = [
+  {
+    'id': 1,
+    'image': 'https://placeimg.com/64/64/1',
+    'name': '김지영1',
+    'birthday': '19900103',
+    'gender': '여자1',
+    'job': '회사원1'
+  },
+  {
+    'id': 2,
+    'image': 'https://placeimg.com/64/64/2',
+    'name': '김지영2',
+    'birthday': '19900103',
+    'gender': '여자2',
+    'job': '회사원2'
+  },
+  {
+    'id': 3,
+    'image': 'https://placeimg.com/64/64/3',
+    'name': '김지영3',
+    'birthday': '19900103',
+    'gender': '여자3',
+    'job': '회사원3'
+  }
+]
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        {
+          customers.map(c => {
+            return (
+              <Customer
+                key={c.id}
+                id={c.id}
+                name={c.name}
+                image={c.image}
+                birthday={c.birthday}
+                gender={c.gender}
+                job={c.job}
+              />
+            );
+          })
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
